1977843d8c499875a081ba8e5c68b5745c84e569
Muskan4556 <muskan35163@gmail.com>
2024-09-14 01:13:33 +0530

    add authentication and use Auth0

---
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -0,0 +1,41 @@
+import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+  const clientID = import.meta.env.VITE_AUTH0_CLIENT;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
+
+  if (!domain || !clientID || !redirectUri) {
+    throw new Error("Unable to initialize Auth");
+  }
+
+  // This function is called after the user successfully authenticates and is redirected back to the application.
+  const onRedirectCallback = (appState?: AppState, user?: User) => {
+    console.log("User: ", user);
+  };
+
+  return (
+    <Auth0Provider
+      domain={domain}
+      clientId={clientID}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+      }}
+      onRedirectCallback={onRedirectCallback}
+    >
+      {children}
+    </Auth0Provider>
+  );
+};
+
+export default Auth0ProviderWithNavigate;
+
+/**
+ * authorizationParams.redirect_uri: The URL to where you'd like to redirect your users after they authenticate with Auth0.
+ * import.meta.env: This is used to access environment variables in Vite projects.
+ *  VITE_AUTH0_DOMAIN, CLIENT, VITE_AUTH0_CALLBACK_URL: These are environment variables that store your Auth0 domain, client ID, and redirect URL respectively.
+ */
--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,9 +1,15 @@
+import { useAuth0 } from "@auth0/auth0-react";
 import { Button } from "./ui/button";
 
 const MainNav = () => {
+  const { loginWithRedirect } = useAuth0();
   return (
     <div>
-      <Button variant="ghost" className="font-bold hover:text-orange-500 hover:bg-white text-black/60 text-md" >
+      <Button
+        variant="ghost"
+        className="font-bold hover:text-orange-500 hover:bg-white text-black/60 text-md"
+        onClick={async () => await loginWithRedirect()}
+      >
         Log In
       </Button>
     </div>
@@ -11,3 +17,7 @@ const MainNav = () => {
 };
 
 export default MainNav;
+
+/**
+ * loginWithRedirect - Type: () => Promise<void>
+ */
